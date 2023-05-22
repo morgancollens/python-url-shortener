7516a1b0de40e13fdde265394d8647b0e64e0601
Morgan Collens <morgan@criticalpanda.com>
2023-05-22 09:04:06 -0700

    Initial commit: added setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='python-url-shortener',
+    version='1.0.0',
+    author='Morgan Collens',
+    description='A simple project designed to help learn the fundamentals of python. Built using both Python and Flask, this service is designed to allow a user to shorten a url and access the original URL via the shortened version.',
+    packages=['python-url-shortener'],
+    install_requires=[],
+)
